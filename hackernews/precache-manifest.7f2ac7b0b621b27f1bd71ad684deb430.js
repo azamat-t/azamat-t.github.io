7aa32a4f8336b670e91ab5c0039a4f5ade904c91
azamattugelbay <azamattugelbay@gmail.com>
2019-02-18 01:18:12 +0600

    Hacker News

---
+++ hackernews/precache-manifest.7f2ac7b0b621b27f1bd71ad684deb430.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1f956bfc8eb1fc4a1ca",
+    "url": "/hackernews/static/js/runtime~main.f1f956bf.js"
+  },
+  {
+    "revision": "cc1c28aa9e1290d14e94",
+    "url": "/hackernews/static/js/main.cc1c28aa.chunk.js"
+  },
+  {
+    "revision": "c7438169e650b60dfca7",
+    "url": "/hackernews/static/js/2.c7438169.chunk.js"
+  },
+  {
+    "revision": "cc1c28aa9e1290d14e94",
+    "url": "/hackernews/static/css/main.83d9bab6.chunk.css"
+  },
+  {
+    "revision": "f31d0157c8e6254efaa4db2d32d251b5",
+    "url": "/hackernews/index.html"
+  }
+];
